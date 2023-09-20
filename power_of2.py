16e14c7f22431ab9adb0634b5acb508869b16457
Misagh <132880744+CptMisagh@users.noreply.github.com>
2023-09-20 12:16:32 +0330

    first version

---
+++ power_of2.py
@@ -0,0 +1,7 @@
+print("hellooooo \nwelcome to this program......")
+print("I am misagh,this is mini project")
+input("pls press the ENTER key to continue.....")
+n = float(input("pls enter a number:"))
+for number in [n]:
+    print(n**2)
+print("done")
