eeb43793071b10ae81bf88b9fd3e6a1ff5eb8d52
Misagh <132880744+CptMisagh@users.noreply.github.com>
2023-09-20 12:53:22 +0330

    Update power_of2.py
    غلط املایی نام با حرف بزرگ

--- power_of2.py
+++ power_of2.py
@@ -1,5 +1,5 @@
 print("hellooooo \nwelcome to this program......")
-print("I am misagh,this is mini project")
+print("I am Misagh,this is very mini project")
 input("pls press the ENTER key to continue.....")
 n = float(input("pls enter a number:"))
 for number in [n]:
